c22b7c305e38a3340a240516e1400ecbe5ee9e20
shubham827155 <159183014+shubham827155@users.noreply.github.com>
2024-03-07 12:57:12 +0000

    Data types conversion

---
+++ conversion.js
@@ -0,0 +1,18 @@
+// let score="undefined"
+// console.log(typeof(score));
+// console.log(typeof score);
+
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+// let loggedIn=0
+// let booleanloggedIn=Boolean(loggedIn)
+// // console.log(booleanloggedIn);
+
+let someNumber=243
+
+let stringNumber=String(someNumber)
+ console.log(stringNumber);
+
+  console.log(typeof stringNumber);
