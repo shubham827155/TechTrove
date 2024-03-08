fab70b6a7aa36a82afed724e28a1ec8420cf3762
shubham827155 <159183014+shubham827155@users.noreply.github.com>
2024-03-08 18:27:08 +0000

    Complete conversion

--- conversion.js
+++ conversion.js
@@ -9,10 +9,49 @@
 // let loggedIn=0
 // let booleanloggedIn=Boolean(loggedIn)
 // // console.log(booleanloggedIn);
-
 let someNumber=243
 
 let stringNumber=String(someNumber)
- console.log(stringNumber);
+//  console.log(stringNumber);
 
-  console.log(typeof stringNumber);
+//   console.log(typeof stringNumber);
+// _____________________________________________________operations_________________________________________________________________
+
+let val=9
+ let negValue=-val
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str="hello"
+// let str1=" shiva"
+// let str2=str+str1
+// console.log(str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num,num1,num2;
+num=num1=num2=2+9
+let counter=900
+
+
+console.log(counter++);
+console.log(counter);
+
+
+
+
+
+
+
+
