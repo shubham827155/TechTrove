270923cf7d11f204d4636263df75171b4fe64f83
shubham827155 <159183014+shubham827155@users.noreply.github.com>
2024-03-08 19:12:40 +0000

    Summary

---
+++ Basics/dataTypes.js
@@ -0,0 +1,31 @@
+const score=100.99
+
+
+// console.log(typeof score);
+
+const id=Symbol('123')
+ const anotherId=Symbol('123')
+
+//  console.log(id===anotherId);
+
+ const bigNumber=48737860387609840928n
+
+
+ //Reference type(Non-Premitive type)
+//  Array,Objects,Functions
+
+const heros=["dogesh","vimdhayk","Chimtu","Samdar"]
+
+
+let objectNew={name:"Shubham",
+age:22
+}
+
+const myFunction=function(){
+
+    console.log("hello shaitaan is here");
+}
+// function()
+// myFunction()
+
+console.log(typeof id);
